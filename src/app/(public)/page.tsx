4794b574caa3a4413d763dd13924da17fac3d1bd
javohir7777 <javohirjumayev7777@gmail.com>
2023-10-21 04:23:23 -0700

    add to search

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -18,17 +18,7 @@ const Home = async () => {
       <main className="container">
         <div className="carusel-bg">
           <h1 className="text-center">Yangi maxsulot</h1>
-          {/* <Swiper
-            spaceBetween={50}
-            slidesPerView={3}
-            onSlideChange={() => console.log("slide change")}
-            onSwiper={(swiper) => console.log(swiper)}
-          >
-            <SwiperSlide>Slide 1</SwiperSlide>
-            <SwiperSlide>Slide 2</SwiperSlide>
-            <SwiperSlide>Slide 3</SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
-          </Swiper> */}
+          
         </div>
         <div className="todos">
           <div className="category">
